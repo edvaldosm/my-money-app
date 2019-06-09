f44b5ee2ee7d17433d1480d9844eadb381bce7eb
Edvaldo Matos <edvaldo.santiago@gmail.com>
2019-06-09 02:45:47 -0300

    Navegação concluida

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,14 +4,15 @@ import React from 'react';
 import Header from '../common/template/header';
 import SideBar from '../common/template/sidebar';
 import Footer from '../common/template/footer';
+import Routes from './routes';
 
 export default props => (
-  <div className='wrapper'>
-    <Header />
-    <SideBar />
-    <div className='content-wrapper'>
-      <h1>Conteudo</h1>
-    </div>
-    <Footer />
-  </div>
+	<div className='wrapper'>
+		<Header />
+		<SideBar />
+		<div className='content-wrapper'>
+			<Routes />
+		</div>
+		<Footer />
+	</div>
 );
