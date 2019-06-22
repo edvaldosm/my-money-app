c18d27bb36cdc7dee8f41a79b0c6a59483e56936
Edvaldo Santiago de Matos <edvaldo.santiago@gmail.com>
2019-06-21 23:43:24 -0300

    criacao da lista de consulta ao backend

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
 import Tabs from '../common/tab/tabs';
@@ -6,8 +9,14 @@ import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
 import TabContent from '../common/tab/tabContent';
+import { selectTab, showTabs } from '../common/tab/tabActions';
+import List from './billingCycleList';
 
 class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.selectTab('tabList');
+    this.props.showTabs('tabList', 'tabCreate');
+  }
   render() {
     return (
       <div>
@@ -22,7 +31,7 @@ class BillingCycle extends Component {
             </TabsHeader>
             <TabsContent>
               <TabContent id='tabList'>
-                <h1>Lista</h1>
+                <List />
               </TabContent>
               <TabContent id='tabCreate'>
                 <h1>Incluir</h1>
@@ -40,4 +49,9 @@ class BillingCycle extends Component {
     );
   }
 }
-export default BillingCycle;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ selectTab, showTabs }, dispatch);
+export default connect(
+  null,
+  mapDispatchToProps
+)(BillingCycle);
